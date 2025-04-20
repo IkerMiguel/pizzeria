2f9baaf5d042a96a52c506a07835a06286ad4fb5
Emilee Pinzon <pinzonloaiza06@gmail.com>
2025-04-19 23:45:26 -0500

    se creo el create de pizza

---
+++ resources/views/pizzas/index.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Pizzas</title>
+  </head>
+  <body>
+    <div class="container">
+        <h1 class="mt-4">Pizzas</h1>
+
+
+        <a href="{{ route('pizzas.create') }}" class="btn btn-success mb-3">agregar</a>
+
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($pizzas as $pizza)
+                <tr>
+                    <td>{{ $pizza->id }}</td>
+                    <td>{{ $pizza->name }}</td>
+                    <td>
+                        <a href="#" class="btn btn-sm btn-primary">Editar</a>
+                        <a href="#" class="btn btn-sm btn-danger">Eliminar</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>
